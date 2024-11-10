84245f1eb336efe8f889db3ad7cb3673df7632cb
Subhradip Roy <subhrastien@gmail.com>
2024-11-10 18:05:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { scrapeGoogleMapsTitlesAndHref } = require("./scraper");
 const { scrapePoi } = require("./scraper_profile");
 const { scrapeAbout } = require("./scraper_about");
+const { scrapeAbout2 } = require("./scraper_about2");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
@@ -48,6 +49,21 @@ app.get("/about", async (req, res) => {
     }
 
     const data = await scrapeAbout(url);
+    res.json(data);
+  } catch (error) {
+    const errorMessage = `Error: ${error.message}`;
+    res.status(500).send(errorMessage);
+  }
+});
+
+app.get("/about2", async (req, res) => {
+  try {
+    const url = req.query.url;
+    if (!url) {
+      return res.status(400).send("Error: input url is required");
+    }
+
+    const data = await scrapeAbout2(url);
     res.json(data);
   } catch (error) {
     const errorMessage = `Error: ${error.message}`;
